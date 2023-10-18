e80297ff6e01e9b578bf261bbbf1456ff5b3befd
Harysson <haryssonsoares@gmail.com>
2023-10-18 19:44:15 -0300

    feat: badge catalog page

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,7 +1,19 @@
+import { Badge } from "@/components/ui/badge";
+import { ShapesIcon } from "lucide-react";
 import React from "react";
 
 const CatalogPage = () => {
-  return <div>CatalogPage</div>;
+  return (
+    <div className="p-5">
+      <Badge
+        className="gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
+        variant={"outline"}
+      >
+        <ShapesIcon size={16} />
+        Catalogo
+      </Badge>
+    </div>
+  );
 };
 
 export default CatalogPage;
