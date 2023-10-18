198b94e24f43c20f9ebb63b70009bd4f32b869d7
Harysson <haryssonsoares@gmail.com>
2023-10-18 20:48:50 -0300

    feat: ensure go to home page by menu link

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -88,11 +88,17 @@ export const Header = () => {
                 </Button>
               )}
 
-              <Button variant="outline" className="w-full justify-start gap-2">
-                <HomeIcon size={16} />
-                Inícion
-              </Button>
-
+              <SheetClose asChild>
+                <Link href="/">
+                  <Button
+                    variant="outline"
+                    className="w-full justify-start gap-2"
+                  >
+                    <HomeIcon size={16} />
+                    Inícion
+                  </Button>
+                </Link>
+              </SheetClose>
               <Button variant="outline" className="w-full justify-start gap-2">
                 <PercentIcon size={16} />
                 Ofertas
