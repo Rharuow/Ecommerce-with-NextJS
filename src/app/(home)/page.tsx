ec5cdd5a435fc9b8443498b5766f2111cb65b9ee
Harysson <haryssonsoares@gmail.com>
2023-10-18 18:24:20 -0300

    feat: add product list and product item componenets

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,21 +1,32 @@
-"use client";
-
 import Image from "next/image";
 import { Categories } from "./components/Categories";
+import { prismaClient } from "@/lib/prisma";
+import { ProductList } from "./components/ProductList";
 
-export default function Home() {
+export default async function Home() {
+  const deals = await prismaClient.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+  });
+
   return (
-    <main className="p-5">
+    <main>
       <Image
         src="/banner-home-01.png"
         alt="First banner home shoing '55% de desconto esse mês'"
         height={0}
         width={0}
         sizes="100vw"
-        className="h-auto w-full"
+        className="h-auto w-full px-5"
       />
-      <div className="mt-8">
+      <div className="mt-8 px-5">
         <Categories />
+      </div>
+      <div className="mt-8">
+        <ProductList products={deals} />
       </div>
     </main>
   );
--- src/helpers/product.ts
+++ src/helpers/product.ts
@@ -1,6 +1,6 @@
 import { Product } from "@prisma/client";
 
-interface ProductWithTotalPrice extends Product {
+export interface ProductWithTotalPrice extends Product {
   totalPrice: number;
 }
 
@@ -14,6 +14,7 @@ export const computeProductTotalPrice = (
     };
 
   const totalPrice =
+    Number(product.basePrice) -
     Number(product.basePrice) * (product.discountPercentage / 100);
 
   return {
