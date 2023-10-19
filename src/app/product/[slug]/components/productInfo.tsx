8e509c3482fdbed9e9bdb7efe81c2fd3a7682976
Harysson <haryssonsoares@gmail.com>
2023-10-19 08:23:36 -0300

    feat: add info products components

--- src/app/product/[slug]/components/productInfo.tsx
+++ src/app/product/[slug]/components/productInfo.tsx
@@ -2,7 +2,12 @@
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { ProductWithTotalPrice } from "@/helpers/product";
-import { ArrowDownIcon, ArrowLeftIcon, ArrowRightIcon } from "lucide-react";
+import {
+  ArrowDownIcon,
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  TruckIcon,
+} from "lucide-react";
 import React, { useState } from "react";
 
 interface ProductInfoProps {
@@ -61,6 +66,30 @@ export const ProductInfo = ({
         >
           <ArrowRightIcon size={16} />
         </Button>
+      </div>
+
+      <div className="mt-8 flex flex-col gap-3">
+        <h3 className="font-bold">Descrição</h3>
+        <p className="text-justify text-sm opacity-60">{description}</p>
+      </div>
+
+      <Button className="mt-8 font-bold uppercase">
+        Adicionar ao carrinho
+      </Button>
+
+      <div className="mt-5 flex items-center justify-between rounded-lg bg-accent px-5 py-2">
+        <div className="flex items-center gap-2">
+          <TruckIcon />
+          <div className="flex flex-col">
+            <p className="text-xs">
+              Entrega via <span className="font-bold">FSPacket®</span>
+            </p>
+            <p className="text-sm text-[#8162FF]">
+              Envio para <span className="font-bold">todo Brasil</span>
+            </p>
+          </div>
+        </div>
+        <p className="text-xs font-bold">Frete grátis</p>
       </div>
     </div>
   );
