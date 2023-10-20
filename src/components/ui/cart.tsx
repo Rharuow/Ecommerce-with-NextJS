540e424a36db4b2b7a6a7e39f4041a40de4b1eab
Harysson <haryssonsoares@gmail.com>
2023-10-20 08:13:16 -0300

    improvement: scroll products to cart component

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -4,12 +4,13 @@ import { ShoppingCartIcon } from "lucide-react";
 import { CartContext } from "@/provider/cart";
 import { CartItem } from "./cartItem";
 import { Separator } from "./separator";
+import { ScrollArea } from "./scroll-area";
 
 export const Cart = () => {
   const { products, subTotal, total, totalDiscount } = useContext(CartContext);
 
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex h-full flex-col gap-8">
       <Badge
         className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
         variant={"outline"}
@@ -18,15 +19,17 @@ export const Cart = () => {
         Carrinho
       </Badge>
 
-      <div className="flex flex-col gap-5">
-        {products.length > 0 ? (
-          products.map((product) => (
-            <CartItem key={product.id} product={product} />
-          ))
-        ) : (
-          <p className="text-center font-semibold">Carrinho vazio</p>
-        )}
-      </div>
+      <ScrollArea>
+        <div className="flex h-full flex-col gap-5">
+          {products.length > 0 ? (
+            products.map((product) => (
+              <CartItem key={product.id} product={product} />
+            ))
+          ) : (
+            <p className="text-center font-semibold">Carrinho vazio</p>
+          )}
+        </div>
+      </ScrollArea>
 
       <div className="flex flex-col gap-3">
         <Separator />
